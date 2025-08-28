0b2c4333e84241107e8dd1541c832a43b6ce41da
Diana <polush@gmail.com>
2025-08-27 17:05:22 -0700

    Create ping.js

---
+++ frontend/api/ping.js
@@ -0,0 +1,5 @@
+// Quick check that Vercel functions are working at /api
+module.exports = (req, res) => {
+  res.setHeader('content-type', 'application/json');
+  res.status(200).send(JSON.stringify({ ok: true, time: new Date().toISOString() }));
+};
