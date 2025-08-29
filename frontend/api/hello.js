eae26d559e05740ee505d2e7f99da72f2f640137
Diana <polush@gmail.com>
2025-08-29 16:52:30 -0700

    Delete frontend/api/hello.js

--- frontend/api/hello.js
+++
@@ -1,4 +0,0 @@
-module.exports = async (req, res) => {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.status(200).send(JSON.stringify({ ok: true, route: "/api/hello" }));
-};
